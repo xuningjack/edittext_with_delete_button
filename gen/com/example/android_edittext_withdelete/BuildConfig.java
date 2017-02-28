a3d5c593d9464a1038c3e31d65c7f6cd9571b4e9
xuning108986@pwrd.com <xuning108986@pwrd.com>
2017-02-28 13:47:43 +0800

    delete bin gen

--- gen/com/example/android_edittext_withdelete/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.android_edittext_withdelete;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
